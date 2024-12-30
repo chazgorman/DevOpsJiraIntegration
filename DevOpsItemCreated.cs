52e664332c21358a1605ace715c5809ffbb9a75a
Charlie Gorman <Charlie.Gorman@ottawa.ca>
2024-12-30 12:09:47 -0500

    Add function to create new bug in Jira from DevOps new issue webhook. Add utility classes for DevOps and Jira JSON de/serialization.

--- DevOpsItemCreated.cs
+++ DevOpsItemCreated.cs
@@ -9,6 +9,7 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Net.Mime;
 using System.Text;
+using VSCodeFunction;
 
 namespace lms
 {
@@ -21,12 +22,62 @@ namespace lms
             _logger = logger;
         }
 
-        //[Function("DevOpsItemCreated")]
-        //public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
-        //{
-        //    _logger.LogInformation("C# HTTP trigger function processed a request.");
-        //    return new OkObjectResult("Welcome to LMS Azure Functions!");
-        //}
+        public void SendJiraRequest(DevOps root)
+        {
+            Jira jira = new Jira();
+
+            JiraFields jiraFields = new JiraFields();
+            jiraFields.summary = root.resource.fields.SystemTitle;
+            jiraFields.description = root.detailedMessage.text;
+            jiraFields.issuetype = new Issuetype() { name = "Bug" };
+            jiraFields.project = new JiraProject() { key = "CPG" };
+
+            jira.fields = jiraFields;
+
+            string data = JsonConvert.SerializeObject(jira);
+
+            //string data = @"{
+            //    ""fields"": {
+            //        ""project"": {
+            //            ""key"": ""CPG""
+            //        },
+            //        ""summary"": ""Sample Bug"",
+            //        ""description"": ""Creating a bug via REST API"",
+            //        ""issuetype"": {
+            //            ""name"": ""Bug""
+            //        }
+            //    }
+            //}";
+
+            HttpClient client = new HttpClient();
+
+            string token = @"ATATT3xFfGF0P5aTWd8ypqDBmR9o_Gx_HkG867ehaqbTZmT42Eco6P7CGkQZeSy9YvRAK6nHYJFEr9A7PNLvr8_BOeR-SAfNEMpR75P6FYEiyq2daLk8tJ98JGJzJAFABvCY8McEq5v1uErcyt2LAWWY3kSQhRsmcM09J5JW_TeIivd91EaCxus=D5B7CF5C";
+            //Putting the credentials as bytes.
+            byte[] cred = UTF8Encoding.UTF8.GetBytes("chaz.gorman@gmail.com:" + token);
+
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, "https://chazgorman.atlassian.net/rest/api/latest/issue");
+
+            request.Headers.Add("Accept", "application/json");
+
+            //Putting credentials in Authorization headers.
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", Convert.ToBase64String(cred));
+
+            request.Content = new StringContent(data);
+            request.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+            try
+            {
+                HttpResponseMessage response = client.Send(request);
+                //response.EnsureSuccessStatusCode();
+                //string responseBody = response.Content.ReadAsStringAsync();
+
+                Console.WriteLine(response.StatusCode);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
 
         [Function("DevOpsItemCreated")]
         public async Task<IActionResult> Run(
@@ -38,31 +89,43 @@ namespace lms
             string requestBody = new StreamReader(req.Body).ReadToEndAsync().Result;
             dynamic data = JsonConvert.ToString(requestBody);
 
+            DevOps? rootDevOpsItem = JsonConvert.DeserializeObject<DevOps>(requestBody);
+
             string messageContent = $"{data}";
 
-            HttpClient client = new HttpClient();
+            //HttpClient client = new HttpClient();
 
-            var request = new HttpRequestMessage
-            {
-                Method = HttpMethod.Post,
-                RequestUri = new Uri("https://webhook.site/62403fa8-fc74-4249-8520-7f40b646fb3c"),
-                Content = new StringContent(
-                messageContent,
-                Encoding.UTF8,
-                MediaTypeNames.Application.Json), // or "application/json" in older versions
-            };
+            //var request = new HttpRequestMessage
+            //{
+            //    Method = HttpMethod.Post,
+            //    RequestUri = new Uri("https://webhook.site/62403fa8-fc74-4249-8520-7f40b646fb3c"),
+            //    Content = new StringContent(
+            //    messageContent,
+            //    Encoding.UTF8,
+            //    MediaTypeNames.Application.Json), // or "application/json" in older versions
+            //};
 
-            request.Headers.Add("Accept", "application/json");
-            
-            var response = await client.SendAsync(request)
-                .ConfigureAwait(false);
-            response.EnsureSuccessStatusCode();
+            //request.Headers.Add("Accept", "application/json");
 
-            var responseBody = await response.Content.ReadAsStringAsync()
-                .ConfigureAwait(false);
+            //var response = await client.SendAsync(request)
+            //    .ConfigureAwait(false);
+            //response.EnsureSuccessStatusCode();
+
+            //var responseBody = await response.Content.ReadAsStringAsync()
+            //    .ConfigureAwait(false);
 
             //var responseString = await response.Content.ReadAsStringAsync();
-            return new OkObjectResult(responseBody);
+
+            try
+            {
+                SendJiraRequest(rootDevOpsItem);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return new OkObjectResult("OK");
         }
     }
 }
---
+++ Jira.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace VSCodeFunction
+{
+    public class JiraFields
+    {
+        [JsonProperty("project")]
+        [JsonPropertyName("project")]
+        public JiraProject project;
+
+        [JsonProperty("summary")]
+        [JsonPropertyName("summary")]
+        public string summary;
+
+        [JsonProperty("description")]
+        [JsonPropertyName("description")]
+        public string description;
+
+        [JsonProperty("issuetype")]
+        [JsonPropertyName("issuetype")]
+        public Issuetype issuetype;
+    }
+
+    public class Issuetype
+    {
+        [JsonProperty("name")]
+        [JsonPropertyName("name")]
+        public string name;
+    }
+
+    public class JiraProject
+    {
+        [JsonProperty("key")]
+        [JsonPropertyName("key")]
+        public string key;
+    }
+
+    public class Jira
+    {
+        [JsonProperty("fields")]
+        [JsonPropertyName("fields")]
+        public JiraFields fields;
+    }
+
+
+}
